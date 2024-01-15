272a532b7624389186e11b7b3a37135b8840fbdf
timmyades3 <oluwatimileyinadesina623@gmail.com>
2024-01-15 12:37:30 +0100

    changes

--- core/settings.py
+++ core/settings.py
@@ -79,18 +79,18 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-    #  'default': {
-    #    'ENGINE': 'django.db.backends.postgresql',
-    #    'NAME': config('PGDATABASE'),
-    #    'USER': config('PGUSER'),
-    #   'PASSWORD': config('PGPASSWORD'),
-    #    'HOST': config('PGHOST'),
-    #    'PORT': '5432',
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
     # }
+     'default': {
+       'ENGINE': 'django.db.backends.postgresql',
+       'NAME': config('PGDATABASE'),
+       'USER': config('PGUSER'),
+      'PASSWORD': config('PGPASSWORD'),
+       'HOST': config('PGHOST'),
+       'PORT': '5432',
+    }
 }
 
 
