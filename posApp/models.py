e62c9091e35c518c23e51db5118eb8eec04c6f8f
timmyades3 <oluwatimileyinadesina623@gmail.com>
2024-01-26 13:14:45 +0100

    fixes

--- posApp/models.py
+++ posApp/models.py
@@ -7,7 +7,6 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 
 class Category(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.TextField()
     description = models.TextField()
     status = models.IntegerField(default=1) 
@@ -18,7 +17,6 @@ class Category(models.Model):
         return self.name
 
 class Products(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
     code = models.CharField(max_length=100)
     category_id = models.ForeignKey(Category, on_delete=models.CASCADE)
     name = models.TextField()
@@ -32,7 +30,6 @@ class Products(models.Model):
         return self.code + " - " + self.name
 
 class Sales(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
     code = models.CharField(max_length=100)
     sub_total = models.FloatField(default=0)
     grand_total = models.FloatField(default=0)
@@ -47,34 +44,14 @@ class Sales(models.Model):
         return self.code
 
 class salesItems(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    sale_id = models.ForeignKey(Sales, on_delete=models.CASCADE)
-    product_id = models.ForeignKey(Products, on_delete=models.CASCADE)
+    sale_id = models.ForeignKey(Sales,on_delete=models.CASCADE)
+    product_id = models.ForeignKey(Products,on_delete=models.CASCADE)
     price = models.FloatField(default=0)
     qty = models.FloatField(default=0)
     total = models.FloatField(default=0)
-
-# class Employees(models.Model):
-#     code = models.CharField(max_length=100,blank=True) 
-#     firstname = models.TextField() 
-#     middlename = models.TextField(blank=True,null= True) 
-#     lastname = models.TextField() 
-#     gender = models.TextField(blank=True,null= True) 
-#     dob = models.DateField(blank=True,null= True) 
-#     contact = models.TextField() 
-#     address = models.TextField() 
-#     email = models.TextField() 
-#     department_id = models.ForeignKey(Department, on_delete=models.CASCADE) 
-#     position_id = models.ForeignKey(Position, on_delete=models.CASCADE) 
-#     date_hired = models.DateField() 
-#     salary = models.FloatField(default=0) 
-#     status = models.IntegerField() 
-#     date_added = models.DateTimeField(default=timezone.now) 
-#     date_updated = models.DateTimeField(auto_now=True) 
-
-    # def __str__(self):
-    #     return self.firstname + ' ' +self.middlename + ' '+self.lastname + ' '
+    
 # class Category(models.Model):
+#     user = models.ForeignKey(User, on_delete=models.CASCADE)
 #     name = models.TextField()
 #     description = models.TextField()
 #     status = models.IntegerField(default=1) 
@@ -85,6 +62,7 @@ class salesItems(models.Model):
 #         return self.name
 
 # class Products(models.Model):
+#     user = models.ForeignKey(User, on_delete=models.CASCADE)
 #     code = models.CharField(max_length=100)
 #     category_id = models.ForeignKey(Category, on_delete=models.CASCADE)
 #     name = models.TextField()
@@ -98,6 +76,7 @@ class salesItems(models.Model):
 #         return self.code + " - " + self.name
 
 # class Sales(models.Model):
+#     user = models.ForeignKey(User, on_delete=models.CASCADE)
 #     code = models.CharField(max_length=100)
 #     sub_total = models.FloatField(default=0)
 #     grand_total = models.FloatField(default=0)
@@ -112,8 +91,10 @@ class salesItems(models.Model):
 #         return self.code
 
 # class salesItems(models.Model):
-#     sale_id = models.ForeignKey(Sales,on_delete=models.CASCADE)
-#     product_id = models.ForeignKey(Products,on_delete=models.CASCADE)
+#     user = models.ForeignKey(User, on_delete=models.CASCADE)
+#     sale_id = models.ForeignKey(Sales, on_delete=models.CASCADE)
+#     product_id = models.ForeignKey(Products, on_delete=models.CASCADE)
 #     price = models.FloatField(default=0)
 #     qty = models.FloatField(default=0)
 #     total = models.FloatField(default=0)
+
