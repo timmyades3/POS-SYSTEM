99eb776039ef7a377140d91f319a31ad314b5084
timmyades3 <oluwatimileyinadesina623@gmail.com>
2024-02-06 09:25:25 +0100

    fixed

--- posApp/views.py
+++ posApp/views.py
@@ -99,35 +99,33 @@ from django.contrib import auth
 #     logout(request)
 #     return redirect("/")
 # Create your views here.
-# @login_required
+@login_required
 def home(request):
-    # now = datetime.now()
-    # current_year = now.strftime("%Y")
-    # current_month = now.strftime("%m")
-    # current_day = now.strftime("%d")
-    # categories = len(Category.objects.all())
-    # products = len(Products.objects.all())
-    # transaction = len(Sales.objects.filter(
-    #     date_added__year=current_year,
-    #     date_added__month = current_month,
-    #     date_added__day = current_day
-    # ))
-    # today_sales = Sales.objects.filter(
-    #     date_added__year=current_year,
-    #     date_added__month = current_month,
-    #     date_added__day = current_day
-    # ).all()
-    # total_sales = sum(today_sales.values_list('grand_total',flat=True))
-    # context = {
-    #     'page_title':'Home',
-    #     'categories' : categories,
-    #     'products' : products,
-    #     'transaction' : transaction,
-    #     'total_sales' : total_sales,
-    # }
-    return render(request, 'posApp/404page.html',
-                #   context
-                  )
+    now = datetime.now()
+    current_year = now.strftime("%Y")
+    current_month = now.strftime("%m")
+    current_day = now.strftime("%d")
+    categories = len(Category.objects.all())
+    products = len(Products.objects.all())
+    transaction = len(Sales.objects.filter(
+        date_added__year=current_year,
+        date_added__month = current_month,
+        date_added__day = current_day
+    ))
+    today_sales = Sales.objects.filter(
+        date_added__year=current_year,
+        date_added__month = current_month,
+        date_added__day = current_day
+    ).all()
+    total_sales = sum(today_sales.values_list('grand_total',flat=True))
+    context = {
+        'page_title':'Home',
+        'categories' : categories,
+        'products' : products,
+        'transaction' : transaction,
+        'total_sales' : total_sales,
+    }
+    return render(request, 'posApp/home.html', context)
 
 
 def about(request):
